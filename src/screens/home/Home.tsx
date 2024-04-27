041e5faa3f7a2bf9f629d6bbd5445fd0fe6c8bb9
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-04-27 18:23:37 +0530

    Add OrientationLocker component to Home screen and Video component to Player screen

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,12 @@
 import {SafeAreaView, ScrollView} from 'react-native';
 import Slider from '../../components/Slider';
 import React from 'react';
+import {OrientationLocker, PORTRAIT} from 'react-native-orientation-locker';
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-black p-4 h-full w-full">
+      <OrientationLocker orientation={PORTRAIT} />
       <ScrollView showsVerticalScrollIndicator={false}>
         <Slider filter="d" title="Latest" />
         <Slider filter="category/web-series/netflix" title="Netflix" />
--- src/screens/home/Player.tsx
+++ src/screens/home/Player.tsx
@@ -1,16 +1,29 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {HomeStackParamList} from '../../App';
+import {RootStackParamList} from '../../App';
 import {OrientationLocker, LANDSCAPE} from 'react-native-orientation-locker';
+import Video, {VideoRef} from 'react-native-video';
+import {FullscreenOrientationType} from 'react-native-video';
 
-type Props = NativeStackScreenProps<HomeStackParamList, 'Player'>;
+type Props = NativeStackScreenProps<RootStackParamList, 'Player'>;
 
 const Player = ({route}: Props): React.JSX.Element => {
   return (
     <View className="bg-black h-full w-full p-4">
       <OrientationLocker orientation={LANDSCAPE} />
-      <Text>{route.params.stream}</Text>
+      <Video
+        source={{uri: route.params.stream}}
+        fullscreen={true}
+        controls={true}
+        // selectedAudioTrack={{
+        //   //@ts-ignore
+        //   type: 'title',
+        //   value: '2',
+        // }}
+        onAudioTracks={e => console.log('onAudioTracks', e)}
+        style={{flex: 1}}
+      />
     </View>
   );
 };
