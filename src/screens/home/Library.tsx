e8ad6f693da9bcd761b1a6c14139519c7dd366bd
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-04-30 15:20:14 +0530

    Add Library screen component

---
+++ src/screens/home/Library.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Library = () => {
+  return (
+    <View>
+      <Text>Library</Text>
+    </View>
+  );
+};
+
+export default Library;
