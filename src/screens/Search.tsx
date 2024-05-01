9328d94f49c6b904af11b06ad43a950c484a7c56
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-05-01 10:59:53 +0530

    Add Search screen component

---
+++ src/screens/Search.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Search = () => {
+  return (
+    <View className="h-full w-full bg-black flex justify-center items-center">
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default Search;
