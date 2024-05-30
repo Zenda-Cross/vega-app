767f2978cbeaef7ae297e9d86e2084664558b72d
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-05-30 18:41:51 +0530

    refactor: Add Webview support and Update version to 1.3.7 in package.json and build.gradle

---
+++ src/screens/WebView.tsx
@@ -0,0 +1,44 @@
+import {View, Text, SafeAreaView, Linking} from 'react-native';
+import React from 'react';
+import {WebView} from 'react-native-webview';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {HomeStackParamList} from '../App';
+import {MMKV} from '../lib/Mmkv';
+import {MaterialIcons} from '@expo/vector-icons';
+
+type Props = NativeStackScreenProps<HomeStackParamList, 'Webview'>;
+
+const Webview = ({route, navigation}: Props) => {
+  return (
+    <SafeAreaView className="bg-black w-full h-full">
+      <View className="bg-black w-full mt-6 h-16 flex flex-row justify-between p-3 items-center">
+        <Text className="text-white text-lg font-bold">Webview</Text>
+        <View className="flex flex-row items-center gap-5">
+          <MaterialIcons
+            name="open-in-browser"
+            size={24}
+            color="white"
+            onPress={() => {
+              Linking.openURL(MMKV.getString('baseUrl') + route.params.link);
+            }}
+          />
+          <MaterialIcons
+            name="close"
+            size={24}
+            color="white"
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+        </View>
+      </View>
+      <WebView
+        // javaScriptCanOpenWindowsAutomatically={false}
+        javaScriptEnabled={false}
+        source={{uri: MMKV.getString('baseUrl') + route.params.link}}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Webview;
