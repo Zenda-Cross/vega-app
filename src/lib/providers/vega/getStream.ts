7867d1dfd6941f39f833330dba7ef3ba6cb7ee56
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-07-02 16:17:06 +0530

    Revert "refactor: Update version numbers in build.gradle, package-lock.json, and package.json and remove unused dependencies"
    This reverts commit 258ed02774ac61b367fb49aa000cfb0b2b242726.

--- src/lib/providers/vega/getStream.ts
+++ src/lib/providers/vega/getStream.ts
@@ -126,7 +126,7 @@ export async function vegaGetStream(
 
     /////////////////////////////
     // const vLinkRedirectRes = await fetch(
-    //   'https://cors.smashystream.workers.dev/?destination=' +
+    //   '
     //     vLinkRedirect[1].replace(domains[1], domains[2]),
     //   {
     //     headers: headers,
@@ -177,7 +177,7 @@ export async function vegaGetStream(
     console.log('vcloudLink', vcloudLink);
     /////////////////////////////
     // const vcloudRes = await axios(
-    //   'https://cors.smashystream.workers.dev/?destination=' + vcloudLink?.[1],
+    //   'vcloudLink?.[1],
     //   {headers, signal},
     // );
     /////////////////////////////
