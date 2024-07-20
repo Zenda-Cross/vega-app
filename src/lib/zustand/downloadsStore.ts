162a183fa0f351a5367ba4eb2c2a88272dc4feed
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-07-20 09:57:35 +0530

    refactor: Add notifications for download completion and failure

--- src/lib/zustand/downloadsStore.ts
+++ src/lib/zustand/downloadsStore.ts
@@ -1,13 +1,17 @@
 import {create} from 'zustand';
 
 export interface Downloads {
+  currentDownload: string;
   activeDownloads: String[];
-  setActiveDownloads: (download: string) => void;
+  setCurrentDownload: (download: string) => void;
+  setActiveDownloads: (playload: string) => void;
   removeActiveDownloads: (download: string) => void;
 }
 
 const useDownloadsStore = create<Downloads>(set => ({
+  currentDownload: '',
   activeDownloads: [],
+  setCurrentDownload: (download: string) => set({currentDownload: download}),
   setActiveDownloads: (download: string) =>
     set(state => ({
       activeDownloads: state.activeDownloads.includes(download)
