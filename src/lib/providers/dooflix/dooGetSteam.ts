30cc689d36e4d49bfe19b37628a17d124c028b3a
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-08-17 12:53:08 +0530

    refactor: Update dooGetStream to use a different User-Agent header and include Connection header

--- src/lib/providers/dooflix/dooGetSteam.ts
+++ src/lib/providers/dooflix/dooGetSteam.ts
@@ -9,8 +9,9 @@ export const dooGetStream = async (link: string): Promise<Stream[]> => {
       type: 'm3u8',
       headers: {
         'User-Agent':
-          'MyRK3Yp5wcT49eyw6vp78Dbz4C7mT8D9eySTK2J97X7jERHigYUGuiguFTUGIYHtXGmDX89eDERTGBVYU78Z347X7jajt77jajt7kw2nd2d8Tk2D7UHtrehTKVC7d9QmTm/7.7 (Linux;Android 14) ExoPlayerLib/2.18.1',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
         Referer: 'https://hdmovie2.moe/',
+        Connection: 'keep-alive',
       },
     });
     console.log('doo streams', streams);
