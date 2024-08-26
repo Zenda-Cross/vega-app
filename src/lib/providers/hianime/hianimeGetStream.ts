670ae2978421a65e608f25615fd36bcf6dd89d5a
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2024-08-26 11:54:20 +0530

    Create hianimeGetStream.ts

---
+++ src/lib/providers/hianime/hianimeGetStream.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {Stream} from '../types';
+
+export const hianimeGetStream = async (id: string): Promise<Stream[]> => {
+  try {
+    const url = `https://private-aniwatch-api.vercel.app/anime/episode-srcs?id=${id}`;
+    const res = await axios.get(url);
+    const data = res.data;
+    const streamLinks: Stream[] = [];
+    data.sources.forEach((source: any) => {
+      streamLinks.push({
+        server: source.quality || 'unknown',
+        link: source.url,
+        type: source.type ? 'm3u8' : 'mp4',
+      });
+    });
+    return streamLinks;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
