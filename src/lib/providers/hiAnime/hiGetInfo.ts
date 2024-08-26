bfb3addd56fb9a5375e2f2902a099369b58e917d
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-08-26 15:49:02 +0530

    refactor: Add HiAnime provider dub support

--- src/lib/providers/hiAnime/hiGetInfo.ts
+++ src/lib/providers/hiAnime/hiGetInfo.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {Info, EpisodeLink} from '../types';
+import {Info, EpisodeLink, Link} from '../types';
 import {getBaseUrl} from '../getBaseUrl';
 
 export const hiGetInfo = async function (link: string): Promise<Info> {
@@ -15,35 +15,59 @@ export const hiGetInfo = async function (link: string): Promise<Info> {
       image: data.image,
       tags: [
         data?.type,
-        data?.subOrDub === 'both' ? 'Sub and Dub' : data?.subOrDub,
+        data?.subOrDub === 'both' ? 'Sub And Dub' : data?.subOrDub,
       ],
       imdbId: '',
       type: data.episodes.length > 0 ? 'series' : 'movie',
     };
 
-    const links: EpisodeLink[] = [];
+    const linkList: Link[] = [];
+    const subLinks: EpisodeLink[] = [];
     data.episodes.forEach((episode: any) => {
       const title = 'Episode ' + episode.number;
       const link = episode.id;
       if (link && title) {
-        links.push({
+        subLinks.push({
           title,
           link,
         });
       }
     });
 
+    linkList.push({
+      title: meta.title + ' (Sub)',
+      episodesLink: '',
+      movieLinks: '',
+      directLinks: subLinks,
+      quality: '',
+    });
+
+    if (data?.subOrDub === 'both') {
+      const dubLinks: EpisodeLink[] = [];
+      data.episodes.forEach((episode: any) => {
+        const title = 'Episode ' + episode.number;
+        const link = episode.id?.replace('both', 'dub');
+        console.log(link);
+        if (link && title) {
+          dubLinks.push({
+            title,
+            link,
+          });
+        }
+      });
+
+      linkList.push({
+        title: meta.title + ' (Dub)',
+        episodesLink: '',
+        movieLinks: '',
+        directLinks: dubLinks,
+        quality: '',
+      });
+    }
+
     return {
       ...meta,
-      linkList: [
-        {
-          episodesLink: '',
-          movieLinks: '',
-          quality: '',
-          title: meta.title,
-          directLinks: links,
-        },
-      ],
+      linkList: linkList,
     };
   } catch (err) {
     console.error(err);
