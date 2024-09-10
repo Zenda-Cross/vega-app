5d57a9aa28285db6c65f33a864bc54ac994593a9
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-09-10 17:01:37 +0530

    Refactor clGetEpisodes to handle encoded links in cinemaLuxe provider

--- src/lib/providers/cinemaLuxe/clGetSteam.ts
+++ src/lib/providers/cinemaLuxe/clGetSteam.ts
@@ -9,17 +9,22 @@ export const clGetStream = async (
   signal: AbortSignal,
 ): Promise<Stream[]> => {
   try {
-    const res = await axios.get(link);
-    const data = res.data;
-    const dlLink = atob(data.match(/"link":"([^"]+)"/)[1]);
-    const res2 = await axios.get(dlLink, {signal});
-    const data2 = res2.data;
-    const newLink = data2.match(/location\.replace\('([^']+)'/)?.[1] || dlLink;
+    let newLink = link;
+    if (!link.includes('hubcloud') && !link.includes('gdflix')) {
+      console.log('link', link);
+      const res = await axios.get(link);
+      const data = res.data;
+      const encodedLink = data.match(/"link":"([^"]+)"/)[1];
+      newLink = encodedLink ? atob(encodedLink) : link;
+    }
     console.log('newLink', newLink);
     if (newLink.includes('gdflix')) {
       const sreams = await gdFlixExtracter(newLink, signal);
       return sreams;
     }
+    const res2 = await axios.get(newLink, {signal});
+    const data2 = res2.data;
+    newLink = data2.match(/location\.replace\('([^']+)'/)?.[1] || newLink;
     const hubCloudLinks = await hubcloudExtracter(newLink, signal);
     return hubCloudLinks;
   } catch (err) {
