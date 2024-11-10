f1c90bc955345b26c296a0c44e9f309d507e58e6
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-11-10 09:27:48 +0530

    Refactor allGetStream and getWhvxStream: streamline whvx stream handling, add token to requests, and improve error handling

--- src/lib/providers/autoEmbed/getWhvxStream.ts
+++ src/lib/providers/autoEmbed/getWhvxStream.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const getWhvxStream = async (
   imdbId: string,
   tmdbId: string,
@@ -25,6 +27,23 @@ export const getWhvxStream = async (
           : '',
       }),
     );
+    const headers = {
+      accept: '*/*',
+      'accept-language': 'en-US,en;q=0.9',
+      'cache-control': 'no-cache',
+      pragma: 'no-cache',
+      priority: 'u=1, i',
+      'sec-ch-ua':
+        '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+      origin: 'https://www.vidbinge.com',
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
+    };
     console.log('searchQuery', {
       title: title,
       imdbId: imdbId,
@@ -42,18 +61,24 @@ export const getWhvxStream = async (
     setTimeout(() => {
       controller.abort();
     }, 4000);
+    const tokenRes = await axios.get('https://ext.8man.me/api/whvxToken', {
+      timeout: 4000,
+    });
+    const tokenJson = tokenRes.data;
+    const token = encodeURIComponent(tokenJson?.token);
+
     const searchRes = await fetch(
-      `${atob(baseUrl)}/search?query=${searchQuery}&provider=${provider}`,
+      `${atob(
+        baseUrl,
+      )}/search?query=${searchQuery}&provider=${provider}&token=${token}`,
       {
-        headers: {
-          'if-none-match': 'W/"d4-7mcv5HTZs5ogd/iJwPMEZ/NGCw0"',
-          origin: atob('aHR0cHM6Ly93d3cudmlkYmluZ2UuY29t'),
-        },
+        headers: headers,
         signal: controller.signal,
         referrerPolicy: 'no-referrer',
         body: null,
-
         method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
       },
     );
     const searchController = new AbortController();
@@ -61,21 +86,25 @@ export const getWhvxStream = async (
       searchController.abort();
     }, 4000);
     const searchJson = await searchRes.json();
-    console.log('whvx', provider, searchQuery);
+    console.log('whvx res', searchJson);
+    console.log(
+      'whvx url',
+      `${atob(
+        baseUrl,
+      )}/search?query=${searchQuery}&provider=${provider}&token=${token}`,
+    );
     const streamRes = await fetch(
       `${atob(baseUrl)}/source?resourceId=${encodeURIComponent(
         searchJson?.url,
       )}&provider=${provider}`,
       {
-        headers: {
-          'if-none-match': 'W/"d4-7mcv5HTZs5ogd/iJwPMEZ/NGCw0"',
-          origin: atob('aHR0cHM6Ly93d3cudmlkYmluZ2UuY29t'),
-        },
+        headers: headers,
+        signal: searchController.signal,
         referrerPolicy: 'no-referrer',
         body: null,
-        signal: searchController.signal,
-
         method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
       },
     );
     const streamJson = await streamRes.json();
