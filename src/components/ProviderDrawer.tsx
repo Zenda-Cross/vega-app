f21a97cb244c6dd6c993138b1251f8808c3c773e
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-12-31 10:30:56 +0530

    Refactor ProviderDrawer and Settings components to use SvgUri for provider flags and enhance UI with BlurView

--- src/components/ProviderDrawer.tsx
+++ src/components/ProviderDrawer.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import useContentStore from '../lib/zustand/contentStore';
 import {providersList} from '../lib/constants';
 import {ScrollView} from 'moti';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import useThemeStore from '../lib/zustand/themeStore';
 import {TouchableOpacity} from 'react-native';
 import {DrawerLayout} from 'react-native-gesture-handler';
+import {BlurView} from 'expo-blur';
+import {Ionicons} from '@expo/vector-icons';
+import {SvgUri} from 'react-native-svg';
 
 const ProviderDrawer = ({
   drawerRef,
@@ -17,12 +19,20 @@ const ProviderDrawer = ({
   const {primary} = useThemeStore(state => state);
 
   return (
-    <View className="px-3">
+    <BlurView
+      intensity={90}
+      experimentalBlurMethod="dimezisBlurView"
+      // renderToHardwareTextureAndroid={true}
+      blurReductionFactor={5}
+      tint="dark"
+      className="px-3">
       <View className="mt-6 p-2 flex flex-row justify-center items-center gap-x-3">
-        <MaterialIcons name="extension" size={24} color={primary} />
+        <Ionicons name="extension-puzzle-outline" size={24} color={primary} />
         <Text className="text-white text-2xl">Providers</Text>
       </View>
-      <ScrollView showsVerticalScrollIndicator={false} className="mb-20">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="mb-20 flex gap-[0.8]">
         {providersList.map(item => (
           <TouchableOpacity
             key={item.value}
@@ -30,19 +40,20 @@ const ProviderDrawer = ({
               setProvider(item);
               drawerRef.current?.closeDrawer();
             }}
-            className={`bg-black text-white w-48 flex-row justify-start gap-2 items-center px-4 py-1 rounded-s-md border-b border-white/10 border rounded-md ${
-              provider.value === item.value ? 'bg-quaternary' : ''
-            } ${item.value === 'lux' ? 'pb-1' : 'pb-3'}`}>
+            className={`text-white w-44 flex-row justify-start gap-2 items-center px-3 py-1 rounded-s-md border-b border-white/10 rounded-md ${
+              provider.value === item.value ? 'bg-white/10' : ''
+            } `}>
+            <SvgUri className="mb-2" width={20} height={20} uri={item.flag} />
             <Text className=" text-white mb-2">
-              {item.flag}
-              &nbsp; &nbsp;
+              {/* {item.flag} */}
+              {/* &nbsp; &nbsp; */}
               {item.name}
             </Text>
           </TouchableOpacity>
         ))}
         <View className="h-16" />
       </ScrollView>
-    </View>
+    </BlurView>
   );
 };
 
