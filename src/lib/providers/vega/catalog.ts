55a3357c2ad03b066c0d31553a90d5001e638760
himanshu8443 <sangwanhimanshu8443@gmail.com>
2025-01-02 12:17:49 +0530

    Update filter paths in catalog provider for consistency

--- src/lib/providers/vega/catalog.ts
+++ src/lib/providers/vega/catalog.ts
@@ -5,97 +5,97 @@ export const homeList = [
   },
   {
     title: 'Netflix',
-    filter: '/web-series/netflix',
+    filter: 'web-series/netflix',
   },
   {
     title: 'Amazon Prime',
-    filter: '/web-series/amazon-prime-video',
+    filter: 'web-series/amazon-prime-video',
   },
   {
     title: '4K Movies',
-    filter: '/movies-by-quality/2160p',
+    filter: 'movies-by-quality/2160p',
   },
 ];
 
 export const genresList = [
   {
     title: 'Action',
-    filter: 'category/movies-by-genres/action/',
+    filter: 'category/movies-by-genres/action',
   },
   {
     title: 'Adventure',
-    filter: 'category/movies-by-genres/adventure/',
+    filter: 'category/movies-by-genres/adventure',
   },
   {
     title: 'Animation',
-    filter: 'category/movies-by-genres/animation/',
+    filter: 'category/movies-by-genres/animation',
   },
   {
     title: 'Biography',
-    filter: 'category/movies-by-genres/biography/',
+    filter: 'category/movies-by-genres/biography',
   },
   {
     title: 'Comedy',
-    filter: 'category/movies-by-genres/comedy/',
+    filter: 'category/movies-by-genres/comedy',
   },
   {
     title: 'Crime',
-    filter: 'category/movies-by-genres/crime/',
+    filter: 'category/movies-by-genres/crime',
   },
   {
     title: 'Documentary',
-    filter: 'category/movies-by-genres/documentary/',
+    filter: 'category/movies-by-genres/documentary',
   },
   {
     title: 'Drama',
-    filter: 'category/movies-by-genres/drama/',
+    filter: 'category/movies-by-genres/drama',
   },
   {
     title: 'Family',
-    filter: 'category/movies-by-genres/family/',
+    filter: 'category/movies-by-genres/family',
   },
   {
     title: 'Fantasy',
-    filter: 'category/movies-by-genres/fantasy/',
+    filter: 'category/movies-by-genres/fantasy',
   },
   {
     title: 'History',
-    filter: 'category/movies-by-genres/history/',
+    filter: 'category/movies-by-genres/history',
   },
   {
     title: 'Horror',
-    filter: 'category/movies-by-genres/horror/',
+    filter: 'category/movies-by-genres/horror',
   },
   {
     title: 'Music',
-    filter: 'category/movies-by-genres/music/',
+    filter: 'category/movies-by-genres/music',
   },
   {
     title: 'Mystery',
-    filter: 'category/movies-by-genres/mystery/',
+    filter: 'category/movies-by-genres/mystery',
   },
   {
     title: 'Romance',
-    filter: 'category/movies-by-genres/romance/',
+    filter: 'category/movies-by-genres/romance',
   },
   {
     title: 'Sci-Fi',
-    filter: 'category/movies-by-genres/sci-fi/',
+    filter: 'category/movies-by-genres/sci-fi',
   },
   {
     title: 'Sport',
-    filter: 'category/movies-by-genres/sport/',
+    filter: 'category/movies-by-genres/sport',
   },
   {
     title: 'Thriller',
-    filter: 'category/movies-by-genres/thriller/',
+    filter: 'category/movies-by-genres/thriller',
   },
   {
     title: 'War',
-    filter: 'category/movies-by-genres/war/',
+    filter: 'category/movies-by-genres/war',
   },
   {
     title: 'Western',
-    filter: 'category/movies-by-genres/western/',
+    filter: 'category/movies-by-genres/western',
   },
 ];
