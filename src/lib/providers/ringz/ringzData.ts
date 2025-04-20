9399dff8f03abd4a9b06c5d1a501e8aa2079ce9e
himanshu8443 <sangwanhimanshu8443@gmail.com>
2025-04-20 13:53:42 +0530

    Refactor ringzData to use a consistent BASE_URL and centralized headers for API requests

--- src/lib/providers/ringz/ringzData.ts
+++ src/lib/providers/ringz/ringzData.ts
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
+export const headers = {
+  'cf-access-client-id': '833049b087acf6e787cedfd85d1ccdb8.access',
+  'cf-access-client-secret':
+    '02db296a961d7513c3102d7785df4113eff036b2d57d060ffcc2ba3ba820c6aa',
+};
+
+const BASE_URL = 'https://privatereporz.pages.dev';
 export async function getRingzMovies() {
   try {
-    const response = await axios.get(
-      'https://ksnvbhsbvujcadgbvui.pages.dev/test.json',
-      {
-        timeout: 4000,
+    const response = await axios.get(`${BASE_URL}/test.json`, {
+      timeout: 4000,
+      headers: {
+        ...headers,
       },
-    );
+    });
     return response.data.AllMovieDataList;
   } catch (error) {
     console.error(error);
@@ -16,12 +23,12 @@ export async function getRingzMovies() {
 
 export async function getRingzShows() {
   try {
-    const response = await axios.get(
-      'https://ksnvbhsbvujcadgbvui.pages.dev/srs.json',
-      {
-        timeout: 4000,
+    const response = await axios.get(`${BASE_URL}/srs.json`, {
+      timeout: 4000,
+      headers: {
+        ...headers,
       },
-    );
+    });
     return response.data.webSeriesDataList;
   } catch (error) {
     console.error(error);
@@ -30,12 +37,12 @@ export async function getRingzShows() {
 
 export async function getRingzAnime() {
   try {
-    const response = await axios.get(
-      'https://ksnvbhsbvujcadgbvui.pages.dev/anime.json',
-      {
-        timeout: 4000,
+    const response = await axios.get(`${BASE_URL}/anime.json`, {
+      timeout: 4000,
+      headers: {
+        ...headers,
       },
-    );
+    });
     return response.data.webSeriesDataList;
   } catch (error) {
     console.error(error);
@@ -44,12 +51,12 @@ export async function getRingzAnime() {
 
 export async function getRingzAdult() {
   try {
-    const response = await axios.get(
-      'https://ksnvbhsbvujcadgbvui.pages.dev/desihub.json',
-      {
-        timeout: 4000,
+    const response = await axios.get(`${BASE_URL}/desihub.json`, {
+      timeout: 4000,
+      headers: {
+        ...headers,
       },
-    );
+    });
     return response.data.webSeriesDataList;
   } catch (error) {
     console.error(error);
