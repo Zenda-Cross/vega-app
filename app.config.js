6e2ee28cb0d415cf9ca55bc53368ab96222bd913
Himanshu <sangwanhimanshu8443@gmail.com>
2025-10-24 19:12:50 +0530

    feat: bump version to 3.2.5 and update versionCode to 154

--- app.config.js
+++ app.config.js
@@ -92,7 +92,7 @@ module.exports = () => {
       autolinking: {exclude: ['expo-splash-screen']},
       plugins,
       slug: 'vega',
-      version: '3.2.4',
+      version: '3.2.5',
       sdkVersion: '52.0.0',
       userInterfaceStyle: 'dark',
       android: {
@@ -102,7 +102,7 @@ module.exports = () => {
         minSdkVersion: 24,
         edgeToEdgeEnabled: true,
         package: 'com.vega',
-        versionCode: 152,
+        versionCode: 154,
         permissions: [
           'FOREGROUND_SERVICE',
           'FOREGROUND_SERVICE_MEDIA_PLAYBACK',
